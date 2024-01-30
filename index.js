22ee4c393b864dc1505ef66e6e44d104e46eb818
gary <garyliggins@yahoo.com>
2024-01-29 20:29:41 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook, ';
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//slicing the currentuser to get the first character
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+console.log(shortGreeting)
